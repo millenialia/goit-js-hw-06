d96622d382b849a4c1eddbbd77172b233ad86461
millenialia <lenalebo@gmail.com>
2023-04-29 21:56:52 +0200

    task 6 done

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -5,8 +5,10 @@ input.addEventListener('blur', onInputBlur)
 function onInputBlur(event) {
     if (event.currentTarget.value.length == input.dataset.length) {
         event.currentTarget.classList.add('valid')
+        event.currentTarget.classList.remove('invalid')
     }
     else {
         event.currentTarget.classList.add('invalid')
+        event.currentTarget.classList.remove('valid')
     }
 }
---
+++ src/js/task-07.js
@@ -0,0 +1,4 @@
+const sizeControlEl = document.querySelector('#font-size-control')
+const text = document.querySelector('#text')
+
+
