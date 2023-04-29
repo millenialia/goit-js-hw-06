ecca635607e78fb56016cc67d02da09587f4ab3a
millenialia <lenalebo@gmail.com>
2023-04-29 22:06:50 +0200

    task 7

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,4 +1,12 @@
 const sizeControlEl = document.querySelector('#font-size-control')
 const text = document.querySelector('#text')
 
+sizeControlEl.addEventListener('input', onInputSize)
 
+let fontSize = 0
+
+function onInputSize(event) {
+    fontSize = event.currentTarget.value
+    text.style.fontSize = fontSize + 'px'
+    
+}
